ab8bcc184da09a1753be51f273c6a88a9c5a0215
Michaeljogoh <michaeljogoh@gmail.com>
2026-06-15 21:11:50 +0100

    deployment pipline

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello from Express deployment pipeline!</h1>')
+  res.send('<h1>Hello from Express deployment pipeline!</h1>')
 })
 
 const PORT = 8080
